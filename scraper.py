bd6dff9de769bb3c31e7e062b7e702dfa4c79194
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-03-28 12:01:55 -0400

    Added basic Beautiful soup search function.

--- scraper.py
+++ scraper.py
@@ -3,29 +3,51 @@ import urllib
 import csv
 
 def main():
-    url = ""
+    global soup
+
+    url = "http://www.hurricanesports.com/SportSelect.dbml?&DB_OEM_ID=28700&SPID=103777&SPSID=658436" # Men's basketball
 
     # Give scraper the URL
-    url_html = get_url(url)
+    get_html(url)
 
     # Scrape page for student-athlete info. Put in list of dictionaries
-    list_of_dict = scrape(url_html)
+    list_of_dict = scrape(soup)
 
     # Convert list of dictionaries into csv
     csv_file = list_to_csv(list_of_dict)
 
-def get_url(url):
+def get_html(url):
     '''
     Given a URL, this method creates the object needed for soup.
     '''
-    print("Got the URL!")
+    global soup
 
-def scrape(url_html):
+    # Open url
+    html = urllib.urlopen(url)
+
+    # Give html to BeautifulSoup
+    soup = soup(html)
+
+    print("Got the HTML!")
+
+def scrape(soup):
     '''
     Given the object, soup will find the student-athlete info.
     A list of dictionaries will be created. Each dictionary represents
     a student-athlete.
     '''
+    student_athletes = []
+
+    # Look for td with class "subhdr."
+    # This is where the table's headers are.
+    #messy_list = soup.find_all("td", {"class" : "subhdr", "align" : ""})
+
+
+    # Look for td with classes "even" and "odd."
+    # This is where each player is in table.
+    print(soup.find_all("td", {"class" : "odd"}))
+    print(soup.find_all("td", {"class" : "even"}))
+
     print("Scraped!")
 
 def list_to_csv(list_of_dict):
