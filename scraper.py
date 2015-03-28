89bed3e3fc2dc654ce762f0596904cce961b5912
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-03-28 11:08:47 -0400

    Added proper parameters to method skeleton.

--- scraper.py
+++ scraper.py
@@ -1,21 +1,26 @@
 from bs4 import BeautifulSoup as soup
 import urllib
+import csv
 
 def main():
-    # Give scraper the URL
-    get_url()
-    # Scrape page for student-athlete info. Put in list of dictionaries
-    scrape()
-    # Convert list of dictionaries into csv
-    list_to_csv()
+    url = ""
 
-def get_url():
+    # Give scraper the URL
+    url_html = get_url(url)
+
+    # Scrape page for student-athlete info. Put in list of dictionaries
+    list_of_dict = scrape(url_html)
+
+    # Convert list of dictionaries into csv
+    csv_file = list_to_csv(list_of_dict)
+
+def get_url(url):
     '''
     Given a URL, this method creates the object needed for soup.
     '''
     print("Got the URL!")
 
-def scrape():
+def scrape(url_html):
     '''
     Given the object, soup will find the student-athlete info.
     A list of dictionaries will be created. Each dictionary represents
@@ -23,7 +28,7 @@ def scrape():
     '''
     print("Scraped!")
 
-def list_to_csv():
+def list_to_csv(list_of_dict):
     '''
     Given the list of dictionaries, this method will convert the list
     into a csv file. The file will be stored in a folder within the
